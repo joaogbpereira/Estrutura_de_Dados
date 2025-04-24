8637dd073d1dfb19f229ef2de568da116af8510d
joaogbpereira <joaopereiraudf@gmail.com>
2025-04-24 12:01:53 -0300

    Delete Atividades Estrutura de Dados directory

--- Atividades Estrutura de Dados/Orientação a Objetos/Exercicio2.py
+++
@@ -1,6 +0,0 @@
-conta2 = {"numero": '123-5', "titular": "José", "saldo": 200.0, "limite": 1000.0}
-
-print(f'O número da conta é: {conta2["numero"]}')
-print(f'O titular da conta é: {conta2["titular"]}')
-print(f'O saldo da conta é: R${conta2["saldo"]}')
-print(f'O limite da conta é: R${conta2["limite"]}')
